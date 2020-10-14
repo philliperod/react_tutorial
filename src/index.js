f287b725b80cd6fe69ddffa121358d529951edd2
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-14 14:51:15 -0500

    simple list: refactored object into an array to learn how to access them dynamically

--- src/index.js
+++ src/index.js
@@ -6,39 +6,34 @@ import './index.css';
 // a simplier approach for the objects below is putting them in an array
 // then you can iterate over that array for each object
 
-const firstBook = {
-  bookImage: 'https://m.media-amazon.com/images/I/81V8ozIJTVL._AC_UY436_FMwebp_QL65_.jpg',
-  author: 'Robin DiAngelo',
-  title: 'White Fragility'
-};
+const books = [
+  {
+    bookImage: 'https://m.media-amazon.com/images/I/81V8ozIJTVL._AC_UY436_FMwebp_QL65_.jpg',
+    author: 'Robin DiAngelo',
+    title: 'White Fragility'
+  },
 
-const secondBook = {
-  bookImage: 'https://m.media-amazon.com/images/I/81aY1lxk+9L._AC_UY436_FMwebp_QL65_.jpg',
-  author: 'Harper Lee',
-  title: 'To Kill A Mockingbird'
-};
+  {
+    bookImage: 'https://m.media-amazon.com/images/I/81aY1lxk+9L._AC_UY436_FMwebp_QL65_.jpg',
+    author: 'Harper Lee',
+    title: 'To Kill A Mockingbird'
+  }
+];
+
+const names = ['john', 'peter', 'susan'];
 
 function BookList() {
-  return (
-    <section className="booklist">
-      <Book bookImage={firstBook.bookImage} title={firstBook.title} author={firstBook.author} />
-
-      <Book bookImage={secondBook.bookImage} title={secondBook.title} author={secondBook.author} />
-    </section>
-  );
+  return <section className="booklist">{names}</section>;
 }
 
 const Book = (props) => {
-  // const {bookImage, title, author} = props;
-  const {bookImage, title, author, children} = props;
+  const {bookImage, title, author} = props;
 
   return (
     <article className="book">
       <img src={bookImage} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {/* {props.children} */}
-      {children}
     </article>
   );
 };
