5adb568c571cbd9c2cb37b8dafd611167f7ed8d7
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-14 08:14:53 -0500

    refactored to remove nested components and insert those tags in just the book component for a leaner look; all styling went into the CSS file for h4

--- src/index.js
+++ src/index.js
@@ -2,18 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// CSS JSX
-// you can add CSS styles directly with JSX
-// in order to do that you need to add a style attribute for that particular element
+// JSX Javascript
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
       <Book />
     </section>
   );
@@ -22,17 +15,11 @@ function BookList() {
 const Book = () => {
   return (
     <article className="book">
-      <BookImage />
-      <Title />
-      <Author />
+      <img src="https://m.media-amazon.com/images/I/81aY1lxk+9L._AC_UY436_FMwebp_QL65_.jpg" alt="" />
+      <h1>To Kill a Mockingbird</h1>
+      <h4>Harper Lee</h4>
     </article>
   );
 };
-
-const BookImage = () => <img src="https://m.media-amazon.com/images/I/81aY1lxk+9L._AC_UY436_FMwebp_QL65_.jpg" alt="" />;
-
-const Title = () => <h1>To Kill a Mockingbird</h1>;
-const Author = () => <h4 style={{color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>Harper Lee</h4>;
-// when you have a CSS property with multiple words then you must use camelCase
 
 ReactDOM.render(<BookList />, document.getElementById('root'));
