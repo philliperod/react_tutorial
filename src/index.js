cd985714b6d3a5249020c9dae2fa8e233ff74a5a
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-13 20:47:48 -0500

    created CSS file to learn how to implement CSS with React; set CSS resets

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Mini Book Project
-// recreating part of Amazon best seller books listing
+// CSS
 
 function BookList() {
   return (
@@ -11,7 +10,6 @@ function BookList() {
     </section>
   );
 }
-// book list component that has a nested book component which nest a book image component
 
 const Book = () => {
   return (
@@ -22,10 +20,8 @@ const Book = () => {
     </article>
   );
 };
-// book component that has a nested book image component
 
 const BookImage = () => <img src="https://m.media-amazon.com/images/I/81aY1lxk+9L._AC_UY436_FMwebp_QL65_.jpg" alt="" />;
-// book image component
 
 const Title = () => <h1>To Kill a Mockingbird</h1>;
 const Author = () => <h4>Harper Lee</h4>;
