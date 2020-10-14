f340e8d50c7b244555325839854e7683fc22b54b
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-14 07:58:20 -0500

    for css properties with multiple words like margin-top, you must use camelCase; added color, font-size, and margin-top to author

--- src/index.js
+++ src/index.js
@@ -32,11 +32,7 @@ const Book = () => {
 const BookImage = () => <img src="https://m.media-amazon.com/images/I/81aY1lxk+9L._AC_UY436_FMwebp_QL65_.jpg" alt="" />;
 
 const Title = () => <h1>To Kill a Mockingbird</h1>;
-const Author = () => <h4 style={{color: '#617d98'}}>Harper Lee</h4>;
-// in order to setup values for this style, you need to put it as a javascript object within 2 curly braces
-// each property will be setup as javascript values
-// first curly braces tells it is going to be javascript
-// second curly braces tells it is the object with values
-// values for a property will need to be put in quotation because it has to be seen as a javascript value
+const Author = () => <h4 style={{color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>Harper Lee</h4>;
+// when you have a CSS property with multiple words then you must use camelCase
 
 ReactDOM.render(<BookList />, document.getElementById('root'));
