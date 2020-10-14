3939bdb5939b185c74a9939c74e04b31968de412
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-14 14:15:39 -0500

    props children: special property that is located between HTML opening/closing tags and being accessed in the property object

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// Props Destructuring
+// Props Children
+// special property
+// located inbetween the HTML tags
+// displays whatever you put between those tags when invoking a component
 
 const firstBook = {
   bookImage: 'https://m.media-amazon.com/images/I/81V8ozIJTVL._AC_UY436_FMwebp_QL65_.jpg',
@@ -19,13 +22,17 @@ const secondBook = {
 function BookList() {
   return (
     <section className="booklist">
-      <Book bookImage={firstBook.bookImage} title={firstBook.title} author={firstBook.author} />
+      <Book bookImage={firstBook.bookImage} title={firstBook.title} author={firstBook.author}>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </Book>
       <Book bookImage={secondBook.bookImage} title={secondBook.title} author={secondBook.author} />
     </section>
   );
 }
+// the children prop is everything that is rendered between opening/closing tag in a component
+// this example is displaying a description
 
-const Book = ({bookImage, title, author}) => {
+const Book = ({bookImage, title, author, children}) => {
   // const {bookImage, title, author} = props;
 
   return (
@@ -33,9 +40,9 @@ const Book = ({bookImage, title, author}) => {
       <img src={bookImage} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
+      {children}
     </article>
   );
 };
-// you can destructure the object for a more simplier solution to pass them
 
 ReactDOM.render(<BookList />, document.getElementById('root'));
