68226d6c87b3111d3fee37f058a1b666077f4fa3
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-15 07:32:04 -0500

    one method to pass the props in Book component into Booklist via listings.map(); create a destructured object with listings props, insert those props inbetween html tags

--- src/index.js
+++ src/index.js
@@ -4,36 +4,25 @@ import './index.css';
 
 // Proper List
 
-const books = [
+const listings = [
   {
     bookImage: 'https://m.media-amazon.com/images/I/81V8ozIJTVL._AC_UY436_FMwebp_QL65_.jpg',
-    author: 'Robin DiAngelo',
-    title: 'White Fragility'
+    title: 'White Fragility',
+    author: 'Robin DiAngelo'
   },
 
   {
     bookImage: 'https://m.media-amazon.com/images/I/81aY1lxk+9L._AC_UY436_FMwebp_QL65_.jpg',
-    author: 'Harper Lee',
-    title: 'To Kill A Mockingbird'
+    title: 'To Kill A Mockingbird',
+    author: 'Harper Lee'
+  },
+
+  {
+    bookImage: 'https://m.media-amazon.com/images/I/71LPo9FucCL._AC_UY436_FMwebp_QL65_.jpg',
+    title: 'Of Mice and Men',
+    author: 'John Steinbeck'
   }
 ];
-
-function BookList() {
-  return (
-    <section className="booklist">
-      {books.map((book) => {
-        const {bookImage, title, author} = book;
-        return (
-          <div>
-            <h3>{title}</h3>
-            <h6>{author}</h6>
-          </div>
-        );
-      })}
-    </section>
-  );
-}
-// alternate option with destructuring the array
 
 const Book = (props) => {
   const {bookImage, title, author} = props;
@@ -46,5 +35,26 @@ const Book = (props) => {
     </article>
   );
 };
+
+function BookList() {
+  return (
+    <section className="booklist">
+      {listings.map((book) => {
+        const {bookImage, title, author} = book;
+        return (
+          <div>
+            <img src={bookImage} alt="" />≈<h3>{title}</h3>
+            <h6>{author}</h6>
+          </div>
+        );
+        // return <Book book={book}></Book>;
+      })}
+    </section>
+  );
+}
+// question to ask: how to pass the Book(props) in BookList?
+// previously, you could destructure another object in BookList
+// which would be in listings.map(book) which would equal book
+// then use those props inbetween html tags within curly braces
 
 ReactDOM.render(<BookList />, document.getElementById('root'));
